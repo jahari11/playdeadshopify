ccfb42a3550ad9a11d6a41fb7ffbb074e5663d8d
jahari11 <jahari.crawford.young@gmail.com>
2024-07-07 18:14:30 -0400

    windows

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,18 @@
 import "@/styles/globals.css";
-
+import Navbar from "@/Components/Navbar";
 import { CartProvider } from "@/libs/CartContext";
 import { Toaster } from "react-hot-toast";
 import { useRouter } from "next/router";
-import {Oswald } from "next/font/google";
+import { Oswald } from "next/font/google";
 import { AnimatePresence, motion } from "framer-motion";
 
-const inter = Oswald({ subsets: ["latin"], weight: '400' });
+const oswald = Oswald({ subsets: ["latin"], weight: '400' });
 
 const pageVariants = {
   initial: {
     opacity: 0,
     y: 20,
   },
-
   enter: {
     opacity: 1,
     y: 0,
@@ -22,33 +21,36 @@ const pageVariants = {
       ease: "easeInOut",
     },
   },
-
   exit: {
-    opacity: 0, 
+    opacity: 0,
     y: -20,
     transition: {
       duration: 0.5,
       ease: "easeInOut",
     },
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
-  return <main className={`${inter.className}`}>
-    <CartProvider>
-    
-    <AnimatePresence mode="wait">
-      <motion.div
-      key={router.pathname}
-      initial="initial"
-      animate="enter"
-      exit="exit"
-      variants={pageVariants}>
-      <Component {...pageProps}/>
-      </motion.div>
-    </AnimatePresence>
-      <Toaster />
-    </CartProvider>
-  </main>
+  return (
+    <main className={`${oswald.className}`}>
+      <CartProvider>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={router.pathname}
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            variants={pageVariants}
+          >
+            <Navbar />
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+        <Toaster />
+      </CartProvider>
+    </main>
+  );
 }
+
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
 <>
     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-    <Navbar />
       <div className="mx-auto relative flex flex-col">
         <div className="flex-grow">
           <h1 className="text-center text-green-600 text-[50px] uppercase font-bold mb-8">playdead Worldwide</h1>
