b37c34b0dca99b3fd35f2f3e8133df5c94774077
Jahari <jahari.crawford.young@gmail.com>
2024-07-07 18:20:45 -0400

    updated shop

--- pages/products/index.js
+++ pages/products/index.js
@@ -6,7 +6,7 @@ export default function Shop({ products }) {
   return (
     <>
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className=" text-center text-white uppercase mb-8 text-5xl">Products</h2>
+        <h2 className=" text-center text-white uppercase mb-8 text-5xl">Shop</h2>
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <Link key={product.id} href={`/products/${product.handle}`} className="group">
